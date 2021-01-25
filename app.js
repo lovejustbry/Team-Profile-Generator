2fc79af7c69bf191fe8f9c83cb71f2c6dafddf75
BrySondra Williams <bry@BrySondrasMBP9.attlocal.net>
2021-01-24 23:51:12 -0600

    fixes errors from tests, updated questions for inquirer

--- app.js
+++ app.js
@@ -4,22 +4,20 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./lib/htmlRenderer");
+const questions  = require("./questions")
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+const Team = [];
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 inquirer
-    .prompt ([
-        type: 'input',
-      message: 'What is the title of your project?',
-      name: 'title'
-    ])
+    
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -31,6 +29,11 @@ inquirer
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
+    fs.writeFile(outputPath, render(Team), 'utf8', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
--- questions.js
+++ questions.js
@@ -1,32 +1,100 @@
 const inquirer = require('inquirer');
 
-employee questions =
-
-{
+const employeeQuestions = {
     type: 'list',
-    message: ''
-    choices: []
-    name: ''
+    message: "What is the employee's role?",
+    choices: ['Manager','Engineer','Intern'],
+    name: 'employeeChoice',
 }
 
-Manager questions =
+const managerQuestions = [
 {
     type: 'input',
-    message: "What is the Manager's name?"
-    name: 'managerName'
+    message: "What is the Manager's name?",
+    name: 'managerName',
 },
 {
     type: 'input',
-    message: "What is the Manager's ID number?"
-    name: 'managerID'
+    message: "What is the Manager's ID number?",
+    name: 'managerID',
 },
 {
     type: 'input',
-    message: "What is the Manager's email?'"
-    name: 'managerEmail'
+    message: "What is the Manager's email?",
+    name: 'managerEmail',
 },
 {
     type: 'input',
-    message: "What is the Manager's office phone number?"
-    name: 'managerPhone'
+    message: "What is the Manager's office phone number?",
+    name: 'managerPhone',
+}
+];
+
+const engineerQuestions = [
+
+{
+    type: 'input',
+    message: "What is the Engineer's name?",
+    name: 'engineerName',
+},
+{
+    type: 'input',
+    message: "What is the Engineer's ID number?",
+    name: 'engineerID',
+},
+{
+    type: 'input',
+    message: "What is the Engineer's email?",
+    name: 'engineerEmail',
+},
+{
+    type: 'input',
+    message: "What is the Engineer's github username?",
+    name: 'engineerGithub',
+}
+];
+const internQuestions = [
+
+{
+    type: 'input',
+    message: "What is the Intern's name?",
+    name: 'internName',
+},
+{
+    type: 'input',
+    message: "What is the Intern's ID number?",
+    name: 'internID',
+},
+{
+    type: 'input',
+    message: "What is the Intern's email?",
+    name: 'internEmail',
+},
+{
+    type: 'input',
+    message: "What is the name of the Intern's University?",
+    name: 'internSchool',
+}
+];
+
+const addTeamMembers = [
+    {
+    type: 'confirm',
+    message: 'Would you like to add more team members?',
+    name: 'addTeamMembers',
+    },
+    {
+        type: 'list',
+        message: 'What type of team member would like to add?',
+        choices: ['Engineer','Intern'],
+        name: 'addTeamMembers',
+    },
+]
+
+module.exports = {
+    employeeQuestions, 
+    engineerQuestions, 
+    managerQuestions, 
+    internQuestions, 
+    addTeamMembers
 }
